74e0a39a8818bdaeb360dffae0f6e24f593436db
MOVS Laboratory <lab.movs@outlook.com>
2021-02-15 18:08:04 +0500

    first №1

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-import json
+import os
+import sys
 
+import pygame
+import requests
 
-if __name__ == '__main__':
+map_request = "https://static-maps.yandex.ru/1.x/?ll=2.294363,48.857501&l=map&spn=0.002,0.0031"
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
     pass
+pygame.quit()
+
+os.remove(map_file)
