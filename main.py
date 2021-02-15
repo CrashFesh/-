b94704e5a5bc818c5d3e0bb2d9cf0519aeeabfbf
MOVS Laboratory <lab.movs@outlook.com>
2021-02-15 18:29:05 +0500

    second №2

--- main.py
+++ main.py
@@ -4,25 +4,45 @@ import sys
 import pygame
 import requests
 
-map_request = "https://static-maps.yandex.ru/1.x/?ll=2.294363,48.857501&l=map&spn=0.002,0.0031"
-response = requests.get(map_request)
+FPS = 120
 
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
-
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+spn_arg = 0.003
 
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map_file), (0, 0))
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_PAGEUP:
+            if spn_arg < 50:
+                spn_arg *= 2
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_PAGEDOWN:
+            if spn_arg > 0.002:
+                spn_arg /= 2
+    map_request = f"https://static-maps.yandex.ru/1.x/?ll=2.294363,48.857501&l=map&spn={spn_arg}" \
+                  f",{spn_arg}"
+    response = requests.get(map_request)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        os.remove(map_file)
+        pygame.quit()
+        sys.exit(1)
+
+    print(spn_arg)
+
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    screen.blit(pygame.image.load(map_file), (0, 0))
+
+    clock.tick(FPS)
+    pygame.display.flip()
 pygame.quit()
 
 os.remove(map_file)
